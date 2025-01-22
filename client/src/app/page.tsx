ba51a690b1d357cdf88f12a6c5a17f93ade2380d
jlow64 <justin64low@gmail.com>
2025-01-22 17:53:24 +1300

    Initialized backend + connected with client

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,3 +1,39 @@
+"use client";
+
+import { useQuery } from "@tanstack/react-query";
+
+type ITransaction = {
+  id: number;
+  name: string;
+  price: number;
+};
+
 export default function Home() {
-  return <div className='container'>Home</div>;
+  const classes = {
+    container: "size-full border border-blue-500",
+  };
+  const { isPending, data } = useQuery({
+    queryKey: ["transactionData"],
+    queryFn: async () => {
+      const response = await fetch(
+        `${process.env.NEXT_PUBLIC_API_URL}/api/transaction`
+      );
+      return await response.json();
+    },
+  });
+
+  if (isPending) return <h1>Loading...</h1>;
+
+  return (
+    <div className={classes.container}>
+      <h1>Home</h1>
+      <div>
+        {data.map((el: ITransaction) => (
+          <p key={el.id}>
+            {el.name} costs: {el.price}
+          </p>
+        ))}
+      </div>
+    </div>
+  );
 }
---
+++ server/Program.cs
@@ -0,0 +1,31 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+//services cors
+builder.Services.AddCors(p => p.AddPolicy("corsapp", builder =>
+{
+    builder.WithOrigins("http://localhost:3000").AllowAnyMethod().AllowAnyHeader();
+}));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.UseCors("corsapp");
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
