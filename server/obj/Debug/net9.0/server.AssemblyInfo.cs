5f868ace09c00011e168c2c99156866a9058dd97
jlow64 <justin64low@gmail.com>
2025-02-02 17:03:36 +1300

    gitignore from build

--- server/obj/Debug/net9.0/server.AssemblyInfo.cs
+++ server/obj/Debug/net9.0/server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1daf67a0d264a8c244bd264af3848375986549c8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+99827ad7b5ea73d4185a0e9da1c8c599409f1653")]
 [assembly: System.Reflection.AssemblyProductAttribute("server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
